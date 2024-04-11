544278b5e874bad506803e9eba82860b6193275e
Martin <ouedraogomartin12@yahoo.fr>
2024-04-11 15:58:39 +0200

    password lost controller

--- src/Util/ErrorTypes.php
+++ src/Util/ErrorTypes.php
@@ -4,11 +4,14 @@ namespace App\Util;
 
 class ErrorTypes
 {
-    public const TOO_MANY_ATTEMPTS = 'TooManyAttempts';
+    public const TOO_MANY_CONNECTION_ATTEMPTS = 'TooManyConnectionAttempts';
+    public const TOO_MANY_PASSWORD_ATTEMPTS = 'TooManyPasswordAttempts';
     public const MISSING_ATTRIBUTES = 'MissingAttributes';
+    public const MISSING_EMAIL = 'MissingEmail';
     public const INVALID_EMAIL = 'InvalidEmail';
     public const INVALID_PASSWORD_FORMAT = 'InvalidPasswordFormat';
     public const USER_NOT_FOUND = 'UserNotFound';
+    public const EMAIL_NOT_FOUND = 'EmailNotFound';
     public const ACCOUNT_NOT_ACTIVE = 'AccountNotActive';
     public const UNEXPECTED_ERROR = 'UnexpectedError';
     public const INVALID_DATE_FORMAT = 'InvalidDateFormat';
